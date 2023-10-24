6a90503f17b852e2b9f042f1a8a86f3b58c41b81
smart-programmer <albrns1123488@outlook.com>
2023-10-24 11:52:55 +0300

    second commmit

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 from flask import Flask, request, jsonify
+from PIL import Image
+from io import BytesIO
+import base64
 
 app = Flask(__name__)
 
@@ -13,14 +16,30 @@ def upload_file():
         return jsonify({"error": "No selected file"})
 
     if file:
+        # Get image details
+        image = Image.open(file)
+        image_format = image.format
+        image_size = image.size
+        image_mode = image.mode
+
+        # Convert image to base64
+        image_bytes = BytesIO()
+        image.save(image_bytes, format=image_format)
+        image_base64 = base64.b64encode(image_bytes.getvalue()).decode('utf-8')
+
         file_details = {
             "filename": file.filename,
             "content_type": file.content_type,
-            "content_length": file.content_length
+            "content_length": file.content_length,
+            "image_format": image_format,
+            "image_size": image_size,
+            "image_mode": image_mode,
+            "image_base64": image_base64
         }
+
         return jsonify(file_details)
     else:
         return jsonify({"error": "Invalid file"})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5006)
+    app.run(host='0.0.0.0', port=8080)
